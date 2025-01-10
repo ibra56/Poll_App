9717a2e236da4beda893d7bf2543b75e663de039
Nnyenje Ibrahim <82960501+ibra56@users.noreply.github.com>
2025-01-11 00:21:53 +0300

    Added the vote list view and logic

--- django_prac/urls.py
+++ django_prac/urls.py
@@ -17,11 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path 
 from polls import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("polls/", views.index, name="index"),
-    path("polls/<int:question_id>/", views.detail, name="detail"),
-    # path("<int:question_id>/results/", views.results, name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
